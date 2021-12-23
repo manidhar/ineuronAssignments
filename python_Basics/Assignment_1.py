97412bb157a3cbbdf9882d8af847c9779dc55431
manidhar <manidhar.karnatakam@gmail.com>
2021-12-23 09:13:56 +0530

    Python Basics Assignment 1 completed

---
+++ python_Basics/Assignment_1.py
@@ -0,0 +1,65 @@
+
+#### Answer for Question 1 ####
+
+# *,-,/,+ are expressions and 'hello',-87.8,6 are values.
+
+
+#### Answer for Question 2 ####
+'''
+A variable is basically the name of the location in the primary memory of your computer.
+This can be chosen by you.
+A variable can store everything from strings to numbers.
+A string is the values inside the quotes assigned to a string.
+'''
+
+#### Answer for Question 3 ####
+
+'''INT,FLOAT, STRING, BOOLEAN, LIST, TUPLE, DICTIONARY, SET, RANGE, and COMPLEX are some data types in python'''
+
+#### Answer for Question 4 ####
+
+'''
+An expression is a combination of values, variables, operators, and calls to functions. 
+Expressions need to be evaluated. 
+If you ask Python to print an expression, the interpreter evaluates the expression and displays the result.
+'''
+
+#### Answer for Question 5 ####
+'''
+An expression is any legal combination of symbols that represents a value. For example, 2.9, a + 5, (3 + 5) / 4. 
+A statement is a programming instruction that does something i.e. some action takes place. 
+For example: print("Hello")
+'''
+
+#### Answer for Question 6 ####
+'''
+the value of a variable changes only when it is reassigned. in this case even though we have performed the operation
+as there is no reassignment of the variable. the value of the variable is still 22
+'''
+
+#### Answer for Question 7 ####
+'''
+'spam' + 'spamspam'
+'spam' * 3
+
+Both will return same output
+'''
+
+#### Answer for Question 8 ####
+'''
+A variable cannot start with numbers or symbols other than _.
+'''
+
+### Answer for Question 9 ####
+'''
+to convert to int: int(x)
+to convert to float: float(x)
+to convert to string: str(x)
+'''
+
+#### Answer for Question 10 ####
+'''
+'I have eaten ' + 99 + ' burritos.' throws an error because 99 is not a string.
+Inorder to fix this error, we need to convert 99 to a string.
+I have  eaten ' + str(99) + ' burritos.'
+'''
