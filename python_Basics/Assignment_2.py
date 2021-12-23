064ae56e79237272ab0dcd9f3bda58fa33a3537a
manidhar <manidhar.karnatakam@gmail.com>
2021-12-23 10:51:18 +0530

    Python Basics Assignment 2 completed

---
+++ python_Basics/Assignment_2.py
@@ -0,0 +1,111 @@
+#### Answer for Question 1 ####
+'''
+True
+False
+'''
+
+#### Answer for Question 2 ####
+'''
+The logical operator are and or not
+'''
+
+#### Answer for Question 3 ####
+'''
+True and True
+True and False
+True or True
+True or False
+False and True
+not True
+not False
+'''
+
+#### Answer for Question 4 ####
+'''
+(5 > 4) and (3 == 5) ==> False
+not (5 > 4) ==> False
+(5 > 4) or (3 == 5) ==> True
+not ((5 > 4) or (3 == 5)) ==> False
+(True and True) and (True == False) ==> False
+(not False) or (not True) ==> False
+
+'''
+
+#### Answer for Question 5 ####
+'''
+==
+!=
+>
+<
+>=
+<=
+'''
+
+#### Answer for Question 6 ####
+'''
+we use == to compare the value and = to assign the value
+ex:
+a = 5
+b = 5
+a == b
+'''
+
+#### Answer for Question 7 ####
+'''
+non conditional statements
+if condition blocks
+if else blocks
+'''
+
+#### Answer for Question 8 ####
+'''
+spam = input("Enter value: ")
+if spam == "1" or int(spam) == 1:
+    print("Hello")
+elif spam == "2" or int(spam) == 2:
+    print("Howdy ")
+else:
+    print("Greetings")
+
+'''
+
+#### Answer for Question 9 ####
+'''
+Ctrl + c   to exit the program
+'''
+
+#### Answer for Question 10 ####
+'''
+Ex for break:  output : 0 1 2 3 4
+
+for i in range(10):
+    if i==5:
+        break
+    print(i)
+Ex for continue: output : 0 1 2 3 4 6 7 8 9
+for i in range(10):
+    if i==5:
+        continue
+    print(i)
+
+'''
+
+#### Answer for Question 11 ####
+'''
+All the statements are same
+'''
+
+### Answer for Question 12 ####
+'''
+for i in range(10):
+    print(i)
+i=0
+while i < 10:
+    print(i)
+    i+=1
+'''
+
+#### Answer for Question 13 ####
+'''
+spam.bacon()
+'''
